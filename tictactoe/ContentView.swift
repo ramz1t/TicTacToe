97fd562b17fe20127be7bc80451b4b1e8566d70b
Timur Ramazanov <timurram007@gmail.com>
2023-09-17 13:16:04 +0200

    Initial Commit

---
+++ tictactoe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tictactoe
+//
+//  Created by Timur Ramazanov on 17.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Timur Ramazanov on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
